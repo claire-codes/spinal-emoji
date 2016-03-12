c48655122d0b95f5dd3b0cb57304d519e8746a6a
Claire Parker <claire.parker87@gmail.com>
2016-03-12 22:05:28 +0000

    :sparkling_heart: Test setup

---
+++ test/modelTest.js
@@ -0,0 +1,7 @@
+"use strict";
+
+describe("this should work", function() {
+  it("Should really work", function() {
+    expect(new MyModel()).to.be.an('object');
+  });
+});
---
+++ test/viewTest.js
@@ -0,0 +1,7 @@
+"use strict";
+
+describe("view", function() {
+  it("should be an object", function() {
+    expect(new MyView()).to.be.an('object');
+  });
+});
