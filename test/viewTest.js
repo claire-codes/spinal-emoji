b3dad0a4cb04d3e12e2f9cc403b9a47e62e6dfb2
Claire Parker <claire.parker87@gmail.com>
2016-03-13 22:19:29 +0000

    :no_bicycles: Moar tests

--- test/viewTest.js
+++ test/viewTest.js
@@ -1,7 +1,31 @@
 "use strict";
 
 describe("view", function() {
-  it("should be an object", function() {
-    expect(new MyView()).to.be.an('object');
+
+  var view, mySpy;
+
+  beforeEach(function() {
+    view = new MyView();
+    mySpy = sinon.spy(view,'randomise');
+  });
+
+  it("should be a Backbone View object", function() {
+    expect(view).to.be.an('object');
+    expect(view).to.be.an.instanceof(Backbone.View);
+  });
+
+  it("should have an el", function() {
+    expect(view.$el).to.equal('#blah');
+  });
+
+  it("should have default div tagname", function() {
+    expect(view.$el.tagname.toLowerCase()).to.equal('div');
+  });
+
+  it("should call randomise on button click only", function() {
+    expect(mySpy.calledOnce).not.to.be.ok;
+    view.render();
+    view.$el.find('.btn').trigger('click');
+    expect(mySpy.calledOnce).to.be.ok;
   });
 });
