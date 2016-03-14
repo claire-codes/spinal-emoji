a3d94635c6bcf37a1dcf39df9342f53df69aa7bb
Claire Parker <claire.parker87@gmail.com>
2016-03-14 20:49:09 +0000

    :seedling: Command line testing

--- test/viewTest.js
+++ test/viewTest.js
@@ -5,8 +5,8 @@ describe("view", function() {
   var view, mySpy;
 
   beforeEach(function() {
+    $('body').append('<div id="myView"></div>');
     view = new MyView();
-    mySpy = sinon.spy(view,'randomise');
   });
 
   it("should be a Backbone View object", function() {
@@ -15,17 +15,32 @@ describe("view", function() {
   });
 
   it("should have an el", function() {
-    expect(view.$el).to.equal('#blah');
+    expect(view.el).to.equal('#blah');
   });
 
   it("should have default div tagname", function() {
-    expect(view.$el.tagname.toLowerCase()).to.equal('div');
+    expect(view.tagName.toLowerCase()).to.equal('div');
+  });
+
+  it("should render alright", function() {
+    expect(view.$el.find('.btn').length).to.eq(1);
+  });
+
+  it("should have a model associated with it", function() {
+    expect(view.model).to.be.an.instanceof(Backbone.Model);
   });
 
   it("should call randomise on button click only", function() {
+    mySpy = sinon.spy(MyView.prototype,'randomise');
+    var rando = sinon.spy(MyView.prototype,'render');
+    expect(rando.calledOnce).not.to.be.ok;
+    var testView = new MyView();
     expect(mySpy.calledOnce).not.to.be.ok;
-    view.render();
-    view.$el.find('.btn').trigger('click');
-    expect(mySpy.calledOnce).to.be.ok;
+    testView.render();
+    testView.$el.find('.btn').trigger('click');
+    // expect(mySpy.calledOnce).to.be.ok;
+    expect(rando.calledOnce).to.be.ok;
+    MyView.prototype.randomise.restore();
+    MyView.prototype.render.restore();
   });
 });
